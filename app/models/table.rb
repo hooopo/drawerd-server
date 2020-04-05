ebc70f45fe10a93b7872fc4f3ccec52023f8452a
Hooopo Wang <hoooopo@gmail.com>
2020-04-05 23:28:11 +0800

    clean

--- app/models/table.rb
+++ app/models/table.rb
@@ -59,7 +59,7 @@ class Table < ApplicationRecord
   end
 
   def column_html
-    columns.to_a.sort_by{|x| x.id}.map do |c|
+    columns.to_a.sort_by { |x| x.id }.map do |c|
       c.to_html
     end.join("\n")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: "stack#index"
+  root to: "home#index"
 
   resources :projects do
     member do
