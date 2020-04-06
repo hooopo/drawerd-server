1112a00f51971ea94890f6b9646b4b588cd457c0
Hooopo Wang <hoooopo@gmail.com>
2020-04-07 05:46:42 +0800

    587

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['EMAIL_USERNAME'],
-  :password => ENV['EMAIL_PASSWORD'],
-  :domain => 'drawerd.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 465,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['EMAIL_USERNAME'],
+  :password => ENV['EMAIL_PASSWORD'],
+  :domain => 'drawerd.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
