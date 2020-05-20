b9eb19310c5c4b35fc8113f5ddaff07ce75d2599
Hooopo Wang <hoooopo@gmail.com>
2020-05-20 08:15:45 +0800

    fix csrf token

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PaymentController < ApplicationController
+  skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!
 
   def webhook
