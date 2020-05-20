e8dc6dac1d560475cd9e8a12ae043afde68ad640
Hooopo Wang <hoooopo@gmail.com>
2020-05-20 08:40:07 +0800

    permit!

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -5,7 +5,7 @@ class PaymentController < ApplicationController
   skip_before_action :authenticate_user!
 
   def webhook
-    data = params.permit.to_h
+    data = params.permit!.to_h
     head 400 unless Paddle.verify(data)
     if data["alert_name"] == "subscription_created"
       user = User.find JSON.parse(data["passthrough"])["user_id"]
