aca150545a17012007de6309ea3eb05082040e65
Hooopo Wang <hoooopo@gmail.com>
2020-05-20 08:32:34 +0800

    fix params

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -5,11 +5,11 @@ class PaymentController < ApplicationController
   skip_before_action :authenticate_user!
 
   def webhook
-    params = params.permit.to_h
-    head 400 unless Paddle.verify(params)
-    if params["alert_name"] == "subscription_created"
-      user = User.find JSON.parse(params["passthrough"])["user_id"]
-      user.subscriptions.create!(company: user.company, event_data: params)
+    data = params.permit.to_h
+    head 400 unless Paddle.verify(data)
+    if data["alert_name"] == "subscription_created"
+      user = User.find JSON.parse(data["passthrough"])["user_id"]
+      user.subscriptions.create!(company: user.company, event_data: data)
       render text: :ok
     else
       render text: :unknown
