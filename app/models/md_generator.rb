2c391d484c5cb95839c2961c84f14bf263ad1e0d
Hooopo Wang <hoooopo@gmail.com>
2020-07-03 02:05:07 +0800

    fix

--- app/models/md_generator.rb
+++ app/models/md_generator.rb
@@ -14,10 +14,10 @@ class MdGenerator
       MDTable.convert([%w[name column_type ext_info ref default comment], *table.columns.order("sort asc, id asc").map do |column|
         [
           column.name,
-          column.column_type, 
-          column.ext_info, 
-          column.relationships.map {|rel| "[#{rel.column&.doc_desc}](##{rel.table&.name})" }.join(", "), 
-          column.default_value, 
+          column.column_type,
+          column.ext_info,
+          column.relationships.map { |rel| "[#{rel.column&.doc_desc}](##{rel.table&.name})" }.join(", "),
+          column.default_value,
           column.comment
         ]
       end]).each do |line|
