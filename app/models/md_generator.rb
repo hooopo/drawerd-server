807e021272c43e7a8809b513c8abe0f31aedabe5
Hooopo Wang <hoooopo@gmail.com>
2020-10-22 23:29:46 +0800

    table comment for md

--- app/models/md_generator.rb
+++ app/models/md_generator.rb
@@ -11,6 +11,7 @@ class MdGenerator
     buffer << "# #{project.name}\n\n"
     project.tables.order("id asc").each do |table|
       buffer << "## #{table.name}\n\n"
+      buffer << "#{table.comment}\n\n"
       MDTable.convert([%w[name column_type ext_info ref default comment], *table.columns.order("sort asc, id asc").map do |column|
         [
           column.name,
